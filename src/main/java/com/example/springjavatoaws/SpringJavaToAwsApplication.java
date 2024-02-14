ab9986890ecd0cd7b39b07e017dd3ac65d64c8a2
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-02-14 12:39:48 +0200

    Initial Commit

---
+++ src/main/java/com/example/springjavatoaws/SpringJavaToAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjavatoaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaToAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaToAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springjavatoaws/SpringJavaToAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjavatoaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaToAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
